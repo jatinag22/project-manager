6a9e6be2209737681bb8f83e50d3457eee2e4eba
jatinag22 <48410162+jatinag22@users.noreply.github.com>
2020-05-14 01:07:03 +0530

    update task schema

--- models/task.js
+++ models/task.js
@@ -1,22 +1,54 @@
 const mongoose = require('mongoose')
 
-const taskSchema = new mongoose.Schema({
+const taskFields = {
     title: {
         type: String,
         required: true,
         trim: true
     },
+    description: {
+        type: String,
+        trim: true
+    },
+    due: {
+        type: Date
+    },
     completed: {
         type: Boolean,
         default: false
     },
+    completedAt: {
+        type: Date
+    }
+}
+
+const subtaskScema = new mongoose.Schema({
+    ...taskFields
+}, {
+    timestamps: true
+})
+
+const taskSchema = new mongoose.Schema({
+    ...taskFields,
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
-    }
+    },
+    subtasks: [subtaskScema]
 }, {
     timestamps: true
+})
+
+taskSchema.pre('save', function(next) {
+    if(this.isModified('completed')) {
+        if(this.completed) {
+            this.completedAt = Date.now()
+        } else {
+            this.completedAt = undefined
+        }
+    }
+    next()
 })
 
 const Task = mongoose.model('Task', taskSchema)
