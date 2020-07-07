03a2238c191b1a41787aa49224bf8de9f22a875b
jatinag22 <48410162+jatinag22@users.noreply.github.com>
2020-07-08 00:15:11 +0530

    update member controller - change task to project

--- src/controllers/member.js
+++ src/controllers/member.js
@@ -1,13 +1,13 @@
-const Task = require('../models/task')
+const Project = require('../models/project')
 
 exports.viewMembers = async (req, res, next) => {
     try {
-        const task = await Task.findOne({_id: req.params.id, members: req.user.id})
-        if(!task) {
-            return next({status: 404, message: 'Task not found!'})
+        const project = await project.findOne({_id: req.params.id, members: req.user.id})
+        if(!project) {
+            return next({status: 404, message: 'Project not found!'})
         }
-        await task.populate('members').execPopulate()
-        res.send(task.members)
+        await project.populate('members').execPopulate()
+        res.send(project.members)
     } catch (e) {
         next(e)
     }
@@ -15,19 +15,19 @@ exports.viewMembers = async (req, res, next) => {
 
 exports.updateMembers = async (req, res, next) => {
     try {
-        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
-        if(!task) {
-            return next({status: 404, message: 'Task not found!'})
+        const project = await Project.findOne({_id: req.params.id, owner: req.user._id})
+        if(!project) {
+            return next({status: 404, message: 'Project not found!'})
         }
         if(req.body.operation === 'add') {
-            task.members.addToSet(...req.body.members)
+            project.members.addToSet(...req.body.members)
         } else if(req.body.operation === 'remove') {
             const members = new Set(req.body.members)
-            members.delete(task.owner.toString())
-            task.members = task.members.filter(member => !members.has(member.toString()))
+            members.delete(project.owner.toString())
+            project.members = project.members.filter(member => !members.has(member.toString()))
         }
-        await task.save()
-        res.send(task.members)
+        await project.save()
+        res.send(project.members)
     } catch (e) {
         next(e)
     }
